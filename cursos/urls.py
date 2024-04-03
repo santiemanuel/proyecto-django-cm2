0d62499ed6297258ba49284ab496057d01d322ca
santiemanuel <santiemanuel@gmail.com>
2024-04-03 19:23:22 -0300

    Clase de Formularios

--- cursos/urls.py
+++ cursos/urls.py
@@ -5,6 +5,9 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("cursos/", views.curso_list, name="curso_list"),
     path("cursos/<int:curso_id>/", views.curso_detail, name="curso_detail"),
+    path("cursos/crear/", views.curso_create, name="curso_create"),
+    path("cursos/<int:curso_id>/editar", views.curso_update, name="curso_update"),
+    path("cursos/<int:curso_id>/eliminar", views.curso_delete, name="curso_delete"),
 ]
 
 handler404 = "cursos.views.error_404"
