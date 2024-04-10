d749589be5264d9520a765dfb1240a7000c8bcb4
santiemanuel <santiemanuel@gmail.com>
2024-04-10 17:58:02 -0300

    Corecciones de cursos archivados

--- cursos/urls.py
+++ cursos/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("cursos/", views.curso_list, name="curso_list"),
-    path("cursos/archivados", views.curso_list_archive, name="curso_list_archive"),
+    path("cursos/archivados/", views.curso_list_archive, name="curso_list_archive"),
     path("cursos/<int:curso_id>/", views.curso_detail, name="curso_detail"),
     path("cursos/crear/", views.curso_create, name="curso_create"),
     path("cursos/<int:curso_id>/editar", views.curso_update, name="curso_update"),
